7c3838d006d1db6e58a3775e0e90b3cbefbcdb62
Pradeep-Gopal <pradeepprachu@gmail.com>
2020-05-12 20:10:00 -0400

    Adding first trial of working simulation

--- pradeep/Algorithm/algorithm.cpp
+++ pradeep/Algorithm/algorithm.cpp
@@ -201,7 +201,11 @@ void fp::algorithm::Solve(std::shared_ptr<fp::landbasedrobot> robot)
             std::cout << "\n";
         }
     }
-
+//    std::cerr <<"should be befjfbjefbjefbjeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" <<std::endl;
+//    std::cerr << WallArray[16][17] <<std::endl;
+//    std::cerr << WallArray[32][33] <<std::endl;
+//    std::cerr << WallArray[0][1] <<std::endl;
+//    std::cerr << WallArray[33][34] <<std::endl;
     PrintStack(Stack);
     std::cout << "\n" ;
     while(CheckGoal(x,y)){
@@ -298,9 +302,9 @@ void fp::algorithm::MoveRobot(std::stack<int> path, std::shared_ptr<fp::landbase
 
 
     while (!path.empty()) {
-//    while(true){
-//        fp::API::moveForward();
+        std::cerr <<"\n"<<std::endl;
         std::cout << "path not empty, following path" <<std::endl;
+        std::cerr << "path not empty, following path" <<std::endl;
         coordinate = path.top();
         point = Get_Coordinates(coordinate);
         x = point[0];
@@ -312,10 +316,11 @@ void fp::algorithm::MoveRobot(std::stack<int> path, std::shared_ptr<fp::landbase
         m = point[0];
         n = point[1];
 
+
         //Checking for wall on Left
+        std::cerr << "outside left value = " << fp::API::wallLeft() <<std::endl;
         if (fp::API::wallLeft()) {
-            std::cerr << "inside wall left" <<std::endl;
-            std::cerr << fp::API::wallLeft() << std::endl;
+            std::cerr << "inside wall left = " << fp::API::wallLeft() <<std::endl;
             if (dir == dirc.NORTH) {
                 direc = 'w';
             } else if (dir == dirc.WEST) {
@@ -328,25 +333,36 @@ void fp::algorithm::MoveRobot(std::stack<int> path, std::shared_ptr<fp::landbase
             fp::API::setWall(x, y, direc);
 
             if (dir == dirc.NORTH && x != 0) {
-                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x - 1, y));
+//                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x - 1, y));
+                WallArray[Set_Coordinates(x, y)][Set_Coordinates(x-1, y)] = 1;
             }
 
             if (dir == dirc.EAST && y != 15) {
-                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x, y + 1));
+                std::cerr << "inside wall left east= " << fp::API::wallLeft() <<std::endl;
+                std::cerr << x << "," << y << "    "<< x <<","<< y+1 <<std::endl;
+                std::cerr << Set_Coordinates(x, y) <<std::endl;
+                std::cerr << Set_Coordinates(x, y + 1) <<std::endl;
+                std::cerr << WallArray[33][49] <<std::endl;
+                WallArray[Set_Coordinates(x, y)][Set_Coordinates(x, y + 1)] = 1;
+//                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x, y + 1));
+                std::cerr << "exiting wall left east= " << fp::API::wallLeft() <<std::endl;
             }
             if (dir == dirc.WEST && y != 0) {
-                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x, y - 1));
+//                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x, y - 1));
+                WallArray[Set_Coordinates(x, y)][Set_Coordinates(x, y - 1)] = 1;
             }
             if (dir == dirc.SOUTH && x != 15) {
-                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x + 1, y));
+//                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x + 1, y));
+                WallArray[Set_Coordinates(x, y)][Set_Coordinates(x+1, y)] = 1;
             }
+            std::cerr << "exiting wall left = " << fp::API::wallLeft() <<std::endl;
 
         }
 
+        std::cerr << "outside front value = " << fp::API::wallFront() <<std::endl;
         //Checking for wall on front
         if (fp::API::wallFront()) {
-            std::cerr << "inside wall front" <<std::endl;
-            std::cerr << fp::API::wallFront() << std::endl;
+            std::cerr << "inside wall front = " << fp::API::wallFront() <<std::endl;
 
             if (dir == dirc.NORTH) {
                 direc = 'n';
@@ -359,25 +375,36 @@ void fp::algorithm::MoveRobot(std::stack<int> path, std::shared_ptr<fp::landbase
             }
             fp::API::setWall(x, y, direc);
             if (dir == dirc.NORTH && y != 15) {
-                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x, y + 1));
+//                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x, y + 1));
+                WallArray[Set_Coordinates(x, y)][Set_Coordinates(x, y + 1)] = 1;
+//                std::cerr <<"should be one" <<std::endl;
+//                std::cerr << WallArray[Set_Coordinates(x, y)][Set_Coordinates(x, y + 1)];
             }
 
             if (dir == dirc.EAST && x != 15) {
-                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x + 1, y));
+//                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x + 1, y));
+                WallArray[Set_Coordinates(x, y)][Set_Coordinates(x+1, y)] = 1;
+//                std::cerr <<"should be one" <<std::endl;
+//                std::cerr << WallArray[Set_Coordinates(x, y)][Set_Coordinates(x+1, y )];
             }
             if (dir == dirc.WEST && x != 0) {
-                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x - 1, y));
+//                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x - 1, y));
+                WallArray[Set_Coordinates(x, y)][Set_Coordinates(x-1, y)] = 1;
+//                std::cerr <<"should be one" <<std::endl;
+//                std::cerr << WallArray[Set_Coordinates(x, y)][Set_Coordinates(x-1, y)];
             }
             if (dir == dirc.SOUTH && y != 0) {
-                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x, y - 1));
+                WallArray[Set_Coordinates(x, y)][Set_Coordinates(x, y - 1)] = 1;
+//                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x, y - 1));
+//                std::cerr <<"should be one" <<std::endl;
+//                std::cerr << WallArray[Set_Coordinates(x, y)][Set_Coordinates(x, y - 1)];
             }
         }
 
-
         // Checking for wall on Right
+        std::cerr << "outside right value = " << fp::API::wallRight() <<std::endl;
         if (fp::API::wallRight()) {
-            std::cerr << "inside wall right" <<std::endl;
-            std::cerr << fp::API::wallRight() << std::endl;
+            std::cerr << "inside wall right = " << fp::API::wallRight() <<std::endl;
             if (dir == dirc.NORTH) {
                 direc = 'e';
             } else if (dir == dirc.WEST) {
@@ -390,34 +417,47 @@ void fp::algorithm::MoveRobot(std::stack<int> path, std::shared_ptr<fp::landbase
             fp::API::setWall(x, y, direc);
 
             if (dir == dirc.NORTH && x != 15) {
-                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x + 1, y));
+                WallArray[Set_Coordinates(x, y)][Set_Coordinates(x+1, y)] = 1;
+//                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x + 1, y));
+//                std::cerr <<"should be one" <<std::endl;
+//                std::cerr << WallArray[Set_Coordinates(x, y)][Set_Coordinates(x+1, y)];
             }
 
             if (dir == dirc.EAST && y != 0) {
-                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x, y - 1));
+                WallArray[Set_Coordinates(x, y)][Set_Coordinates(x, y - 1)] = 1;
+//                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x, y - 1));
+//                std::cerr <<"should be one" <<std::endl;
+//                std::cerr << WallArray[Set_Coordinates(x, y)][Set_Coordinates(x, y - 1)];
             }
             if (dir == dirc.WEST && y != 15) {
-                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x, y + 1));
+                WallArray[Set_Coordinates(x, y)][Set_Coordinates(x, y + 1)] = 1;
+//                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x, y + 1));
+//                std::cerr <<"should be one" <<std::endl;
+//                std::cerr << WallArray[Set_Coordinates(x, y)][Set_Coordinates(x, y + 1)];
             }
             if (dir == dirc.SOUTH && x != 0) {
-                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x - 1, y));
+                WallArray[Set_Coordinates(x, y)][Set_Coordinates(x-1, y)] = 1;
+//                WallArray = SetWall(Set_Coordinates(x, y), Set_Coordinates(x - 1, y));
+//                std::cerr <<"should be one" <<std::endl;
+//                std::cerr << WallArray[Set_Coordinates(x, y)][Set_Coordinates(x-1, y)];
             }
 
         }
 
+
         if (dir == dirc.NORTH) {
             // Moving Right
-            std::cerr << "inside robot motion North" <<std::endl;
+            std::cerr << "inside robot motion  - facing North" <<std::endl;
             std::cout << "prev coordinates = "<< x << y <<std::endl;
             std::cout << "next coordinates = "<< m << n <<std::endl;
 
+            //Moving Right
             if (m > x) {
                 if (fp::API::wallRight()) {
-                    std::cerr << "North inside wall right " <<std::endl;
+                    std::cerr << "North inside wall right = " ;
                     std::cerr << fp::API::wallRight() << std::endl;
                     std::cout << "Wall encountered when moving right, solve function called" << std::endl;
                     std::cerr << "Wall encountered when moving right, solve function called" << std::endl;
-
                     robot->set_x(x);
                     robot->set_y(y);
                     Solve(robot);
@@ -425,6 +465,7 @@ void fp::algorithm::MoveRobot(std::stack<int> path, std::shared_ptr<fp::landbase
                 std::cout << "Moving right" << std::endl;
                 dir = dirc.EAST;
                 fp::API::turnRight();
+                std::cerr << "Moved right" << std::endl;
                 std::cerr << "Current Robot direction = " << dir <<std::endl;
             }
 
@@ -439,6 +480,7 @@ void fp::algorithm::MoveRobot(std::stack<int> path, std::shared_ptr<fp::landbase
                 std::cout << "Moving Left" << std::endl;
                 dir = dirc.WEST;
                 fp::API::turnLeft();
+                std::cerr << "Moved left" << std::endl;
                 std::cerr << "Current Robot direction = " << dir <<std::endl;
             }
 
@@ -452,6 +494,7 @@ void fp::algorithm::MoveRobot(std::stack<int> path, std::shared_ptr<fp::landbase
                 }
                 std::cout << "Moving Forward" << std::endl;
                 dir = dirc.NORTH;
+                std::cerr << "Have to move Forward" <<std::endl;
                 std::cerr << "Current Robot direction = " << dir <<std::endl;
             }
 
@@ -460,13 +503,14 @@ void fp::algorithm::MoveRobot(std::stack<int> path, std::shared_ptr<fp::landbase
             {
                 fp::API::turnRight();
                 fp::API::turnRight();
+                std::cerr << "Rotated right twices" << std::endl;
                 if (fp::API::wallFront()) {
                     std::cout << "Wall encountered when moving backward, solve function called" << std::endl;
                     robot->set_x(x);
                     robot->set_y(y);
                     Solve(robot);
                 }
-                std::cout << "Moving Backward" << std::endl;
+                std::cout << "Have to move backward" << std::endl;
                 dir = dirc.SOUTH;
                 std::cerr << "Current Robot direction = " << dir <<std::endl;
             }
@@ -653,11 +697,11 @@ void fp::algorithm::MoveRobot(std::stack<int> path, std::shared_ptr<fp::landbase
         }
 
         fp::API::moveForward();
-        std::cerr << "Current Robot direction = " << dir <<std::endl;
+//        std::cerr << "Current Robot direction = " << dir <<std::endl;
         std::cout << "Moving forward dawwwww" <<std::endl;
+        std::cerr << "Robot moving forward executed" <<std::endl;
+        std::cerr << "Current Robot coordinates = " << m <<" " << n <<std::endl;
 }
-    std::cerr << "full outside" <<std::endl;
-
 }
 
 
